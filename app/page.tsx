fb952e2788e6b3f5e5bb64a18d5b8ca8e85cf2b1
lagroms <88441919+lagroms@users.noreply.github.com>
2025-05-01 18:34:00 +0400

    assets and metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
-const Home = () => (
-	<h1 className="text-3xl text-white font-black">
-		Welcome to the world of Next.js
-	</h1>
-);
+const Home = () => {
+	return (
+		<>
+			<h1 className="h1-bold">Welcome to the world of Next.js</h1>
+		</>
+	);
+};
 export default Home;

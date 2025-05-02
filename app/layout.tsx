231f09de48d05177547356a20a9d086740190e2a
lagroms <88441919+lagroms@users.noreply.github.com>
2025-05-02 10:15:13 +0400

    added config - linting now working

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,41 @@
 import type { Metadata } from "next";
-import { Inter, Space_Grotesk } from "next/font/google";
+import { Inter, Space_Grotesk as SpaceGrotesk } from "next/font/google";
+import React from "react";
 import "./globals.css";
 
 const inter = Inter({
-	variable: "--font-inter",
-	subsets: ["latin"],
-	weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+    variable: "--font-inter",
+    subsets: ["latin"],
+    weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
 });
 
-const spaceGrotesk = Space_Grotesk({
-	variable: "--font-space-grotesk",
-	subsets: ["latin"],
-	weight: ["300", "400", "500", "600", "700"],
+const spaceGrotesk = SpaceGrotesk({
+    variable: "--font-space-grotesk",
+    subsets: ["latin"],
+    weight: ["300", "400", "500", "600", "700"],
 });
 
 export const metadata: Metadata = {
-	title: "Dev Overflow",
-	description:
-		"A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
-	icons: {
-		icon: "/images/site-logo.svg",
-	},
+    title: "Dev Overflow",
+    description:
+        "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+    icons: {
+        icon: "/images/site-logo.svg",
+    },
 };
 
 export default function RootLayout({
-	children,
+    children,
 }: Readonly<{
-	children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-	return (
-		<html lang="en">
-			<body
-				className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
-			>
-				{children}
-			</body>
-		</html>
-	);
+    return (
+        <html lang="en">
+            <body
+                className={`${inter.className} ${spaceGrotesk.variable} antialiased`}
+            >
+                {children}
+            </body>
+        </html>
+    );
 }
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,21 +1,47 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
+
 import { FlatCompat } from "@eslint/eslintrc";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript", "prettier"),
-	{
-		rules: {
-			"@typescript-eslint/no-unused-vars": "warn",
-		},
-	},
+    ...compat.extends(
+        "next/core-web-vitals",
+        "next/typescript",
+        "standard",
+        "plugin:import/recommended",
+        "plugin:import/typescript",
+        // "plugin:tailwindcss/recommended",
+        "prettier"
+    ),
+    {
+        rules: {
+            "@typescript-eslint/no-unused-vars": "warn",
+            "import/order": [
+                "error",
+                {
+                    groups: [
+                        "builtin",
+                        "external",
+                        "internal",
+                        "parent",
+                        "sibling",
+                        "index",
+                    ],
+                    "newlines-between": "always",
+                    alphabetize: { order: "asc", caseInsensitive: true },
+                },
+            ],
+            "import/no-unresolved": "error",
+            "import/no-duplicates": "error",
+        },
+    },
 ];
 
 export default eslintConfig;

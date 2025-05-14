ade4f64545a44b49b2b909bb126776a0046d632c
lagroms <88441919+lagroms@users.noreply.github.com>
2025-05-14 10:45:30 +0400

    added account, answer, question models

---
+++ database/question.model.ts
@@ -0,0 +1,31 @@
+import { Schema, models, model, Types } from "mongoose";
+
+export interface IQuestion {
+    title: string;
+    content: string;
+    tags: Types.ObjectId[];
+    views: number;
+    upvotes: number;
+    downvotes: number;
+    answers: number;
+    author: Types.ObjectId;
+}
+
+const QuestionSchema = new Schema<IQuestion>(
+    {
+        title: { type: String, required: true },
+        content: { type: String, required: true },
+        tags: [{ type: Schema.Types.ObjectId, required: true, ref: "Tag" }],
+        views: { type: Number, default: 0 },
+        upvotes: { type: Number, default: 0 },
+        downvotes: { type: Number, default: 0 },
+        answers: { type: Number, default: 0 },
+        author: { type: Schema.Types.ObjectId, required: true, ref: "User" },
+    },
+    { timestamps: true }
+);
+
+const Question =
+    models?.question || model<IQuestion>("Question", QuestionSchema);
+
+export default Question;

cf73f4cc44f6b23c6a4c77aa10273a79b049d5d9
lagroms <88441919+lagroms@users.noreply.github.com>
2025-09-08 15:08:36 +0400

    add to collection server action

---
+++ lib/actions/collection.action.ts
@@ -0,0 +1,51 @@
+"use server";
+
+import { revalidatePath } from "next/cache";
+
+import ROUTES from "@/constants/routes";
+import { Collection, Question } from "@/database";
+
+import action from "../handlers/action";
+import handleError from "../handlers/error";
+import { NotFoundError } from "../http-errors";
+import { CollectionBaseSchema } from "../validations";
+
+export async function toggleSaveQuestion(
+    params: CollectionBaseParams
+): Promise<ActionResponse<{ saved: boolean }>> {
+    const validationResult = await action({
+        params,
+        schema: CollectionBaseSchema,
+    });
+
+    if (validationResult instanceof Error) {
+        return handleError(validationResult) as ErrorResponse;
+    }
+
+    const { questionId } = validationResult.params!;
+
+    const userId = validationResult.session?.user?.id;
+
+    try {
+        const question = await Question.findById(questionId);
+        if (!question) {
+            throw new NotFoundError("Question");
+        }
+
+        const collection = await Collection.findOne({
+            question: questionId,
+            author: userId,
+        });
+
+        if (collection) {
+            await Collection.findByIdAndDelete(collection._id);
+            return { success: true, data: { saved: false } };
+        } else {
+            await Collection.create({ question: questionId, author: userId });
+            revalidatePath(ROUTES.QUESTION(questionId));
+            return { success: true, data: { saved: true } };
+        }
+    } catch (error) {
+        return handleError(error) as ErrorResponse;
+    }
+}

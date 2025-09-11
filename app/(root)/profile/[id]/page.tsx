c5093289ea27026754291114f8d017902326a560
lagroms <88441919+lagroms@users.noreply.github.com>
2025-09-11 16:01:24 +0400

    implemented stats ui

--- app/(root)/profile/[id]/page.tsx
+++ app/(root)/profile/[id]/page.tsx
@@ -6,6 +6,7 @@ import { notFound } from "next/navigation";
 import { auth } from "@/auth";
 import { Button } from "@/components/ui/button";
 import ProfileLink from "@/components/user/ProfileLink";
+import Stats from "@/components/user/Stats";
 import UserAvatar from "@/components/UserAvatar";
 import ROUTES from "@/constants/routes";
 import { getUser } from "@/lib/actions/user.action";
@@ -42,58 +43,71 @@ const Profile = async ({ params }: RouteParams) => {
     } = user;
 
     return (
-        <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
-            <div className="flex flex-col items-start gap-4 lg:flex-row">
-                <UserAvatar
-                    id={_id}
-                    name={name}
-                    imageUrl={image}
-                    className="size-[140px] rounded-full object-cover"
-                    fallbackClassName="text-6xl font-bolder"
-                />
-                <div className="mt-3">
-                    <h2 className="h2-bold text-dark100_light900">{name}</h2>
-                    <p className="paragraph-regular text-dark200_light800">
-                        @{username}
-                    </p>
-                    <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
-                        {portfolio && (
-                            <ProfileLink
-                                imgUrl="/icons/link.svg"
-                                title="Portfolio"
-                                href={portfolio}
-                            />
-                        )}
-                        {location && (
-                            <ProfileLink
-                                imgUrl="/icons/location.svg"
-                                title="Location"
-                            />
-                        )}
-                        <ProfileLink
-                            imgUrl="/icons/calendar.svg"
-                            title={format(new Date(createdAt), "MMMM yyyy")}
-                        />
-                    </div>
-
-                    {bio && (
-                        <p className="paragraph-regular text-dark400_light800 mt-8">
-                            {bio}
+        <>
+            <section className="flex flex-col-reverse items-start justify-between sm:flex-row">
+                <div className="flex flex-col items-start gap-4 lg:flex-row">
+                    <UserAvatar
+                        id={_id}
+                        name={name}
+                        imageUrl={image}
+                        className="size-[140px] rounded-full object-cover"
+                        fallbackClassName="text-6xl font-bolder"
+                    />
+                    <div className="mt-3">
+                        <h2 className="h2-bold text-dark100_light900">
+                            {name}
+                        </h2>
+                        <p className="paragraph-regular text-dark200_light800">
+                            @{username}
                         </p>
+                        <div className="mt-5 flex flex-wrap items-center justify-start gap-5">
+                            {portfolio && (
+                                <ProfileLink
+                                    imgUrl="/icons/link.svg"
+                                    title="Portfolio"
+                                    href={portfolio}
+                                />
+                            )}
+                            {location && (
+                                <ProfileLink
+                                    imgUrl="/icons/location.svg"
+                                    title="Location"
+                                />
+                            )}
+                            <ProfileLink
+                                imgUrl="/icons/calendar.svg"
+                                title={format(new Date(createdAt), "MMMM yyyy")}
+                            />
+                        </div>
+
+                        {bio && (
+                            <p className="paragraph-regular text-dark400_light800 mt-8">
+                                {bio}
+                            </p>
+                        )}
+                    </div>
+                </div>
+                <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
+                    {loggedInUser?.user?.id === id && (
+                        <Link href={ROUTES.EDIT_PROFILE}>
+                            <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
+                                <PencilIcon className="size-4" />
+                                Edit Profile
+                            </Button>
+                        </Link>
                     )}
                 </div>
-            </div>
-            <div className="flex justify-end max-sm:mb-5 max-sm:w-full sm:mt-3">
-                {loggedInUser?.user?.id === id && (
-                    <Link href={ROUTES.EDIT_PROFILE}>
-                        <Button className="paragraph-medium btn-secondary text-dark300_light900 min-h-12 min-w-44 px-4 py-3">
-                            <PencilIcon className="size-4" />
-                            Edit Profile
-                        </Button>
-                    </Link>
-                )}
-            </div>
-        </section>
+            </section>
+            <Stats
+                totalQuestions={totalQuestions}
+                totalAnswers={totalAnswers}
+                badges={{
+                    GOLD: 0,
+                    SILVER: 0,
+                    BRONZE: 0,
+                }}
+            />
+        </>
     );
 };
 
